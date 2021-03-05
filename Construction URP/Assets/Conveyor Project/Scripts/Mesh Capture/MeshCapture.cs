12e0d935040fed80051dd799946273e8f9373e69
LaserRock46 <bialyrobert11@gmail.com>
2021-03-05 12:19:14 +0100

    revert

--- Construction URP/Assets/Conveyor Project/Scripts/Mesh Capture/MeshCapture.cs
+++ Construction URP/Assets/Conveyor Project/Scripts/Mesh Capture/MeshCapture.cs
@@ -63,7 +63,7 @@ public class MeshCapture
         {        
             int[] triangle = new int[] {originalTriangles[vertexIndex - 2], originalTriangles[vertexIndex - 1], originalTriangles[vertexIndex] };
             Edge.IndiceLoopOrder[] backLoopVertexIndices = GetBackLoopVertexIndices(triangle,originalVertices);
-            if (backLoopVertexIndices.Length == 2)
+            if (ContainsTwoBackLoops(backLoopVertexIndices))
             {
                 Edge edge = GetEdgeWithWindingOrder(vertexDatas, backLoopVertexIndices,triangle,originalVertices);
                 edges.Add(edge);
@@ -71,22 +71,15 @@ public class MeshCapture
         }
         return edges.ToArray();
     }
-    /*
-    int[] GetBackLoopVertexIndices(int vertexIndex,int[] originalTriangles, Vector3[] originalVertices)
-    {   
-        List<int> backLoopVertexIndices = new List<int>();
-        for (int currentVertexIndex = vertexIndex; currentVertexIndex >= vertexIndex - 2; currentVertexIndex--)
-        {         
-            int originalVertexIndex = originalTriangles[currentVertexIndex];
-            if (!IsOriginalVertexInFront(originalVertices[originalVertexIndex]))
-            {
-                backLoopVertexIndices.Add(originalVertexIndex);
-            }
+    bool ContainsTwoBackLoops(Edge.IndiceLoopOrder[] backLoopVertexIndices)
+    {
+        int count = 0;
+        for (int i = 0; i < backLoopVertexIndices.Length; i++)
+        {
+            if (backLoopVertexIndices[i] == Edge.IndiceLoopOrder.Back) count++;
         }
-      
-        return backLoopVertexIndices.ToArray();
+        return count == 2;
     }
-    */
     Edge.IndiceLoopOrder[] GetBackLoopVertexIndices(int[] triangle, Vector3[] originalVertices)
     {
         Edge.IndiceLoopOrder[] backLoopVertexIndices = new Edge.IndiceLoopOrder[3];
@@ -104,10 +97,81 @@ public class MeshCapture
         VertexData[] triangleFromBackLoop = new VertexData[3];
         VertexData[] triangleFromFrontLoop = new VertexData[3];
 
+        triangleFromBackLoopOrder[0] = backLoopVertexIndices[0];
+        triangleFromBackLoopOrder[1] = backLoopVertexIndices[1];
+        triangleFromBackLoopOrder[2] = backLoopVertexIndices[2];
+      
+        for (int i = 0; i < triangleFromBackLoop.Length; i++)
+        {         
+            triangleFromBackLoop[i] = GetMatchingVertexData(vertexDatas,FrontIndiceToBackIndice(triangleFromBackLoopOrder[i],triangle,originalVertices));
+        }
+
+        var diagonalIndicesFromQuad = GetDiagonalIndicesFromQuad(backLoopVertexIndices, triangle, originalVertices);
+        triangleFromFrontLoopOrder[0] = diagonalIndicesFromQuad.diagonalIndiceLoopOrder[0];
+        triangleFromFrontLoopOrder[1] = diagonalIndicesFromQuad.diagonalIndiceLoopOrder[1];
+        triangleFromFrontLoopOrder[2] = Edge.IndiceLoopOrder.Front;
+        triangleFromFrontLoop[0] = GetMatchingVertexData(vertexDatas,);
+        triangleFromFrontLoop[1] = GetMatchingVertexData(vertexDatas, diagonalIndicesFromQuad.diagonalIndices[1]);
+        int backLoopDiagonalIndice = diagonalIndicesFromQuad.diagonalIndiceLoopOrder[0] == Edge.IndiceLoopOrder.Back ? diagonalIndicesFromQuad.diagonalIndices[0] : diagonalIndicesFromQuad.diagonalIndices[1];
+        triangleFromFrontLoop[2] = GetMatchingVertexData(vertexDatas,backLoopDiagonalIndice);
 
 
-        Edge edge = new Edge(triangleFromBackLoopOrder,triangleFromFrontLoopOrder,triangleFromBackLoop,triangleFromFrontLoop);
-        return edge;
+        return new Edge(triangleFromBackLoopOrder, triangleFromFrontLoopOrder, triangleFromBackLoop, triangleFromFrontLoop);         
+    }
+    int FrontIndiceToBackIndice(Edge.IndiceLoopOrder indiceLoopOrder, int[] triangle, Vector3[] originalVertices)
+    {
+        int backIndice = 0;
+        for (int i = 0; i < triangle.Length; i++)
+        {
+            if (indiceLoopOrder == Edge.IndiceLoopOrder.Back)
+            {
+                backIndice = triangle[i];
+            }
+            else
+            {
+                backIndice = GetVertexInSameLineFromTriangle(triangle[i], triangle, originalVertices);
+            }
+        }
+        return backIndice;
+    }
+    (int[] diagonalIndices, Edge.IndiceLoopOrder[] diagonalIndiceLoopOrder, int backLoopInSameLineIndice) GetDiagonalIndicesFromQuad(Edge.IndiceLoopOrder[] backLoopVertexIndices, int[] triangle, Vector3[] originalVertices)
+    {
+        List<int> diagonalIndices = new List<int>();
+        List<Edge.IndiceLoopOrder> diagonalIndicesLoopOrder = new List<Edge.IndiceLoopOrder>();
+        int backLoopInSameLineIndice = 0;
+
+        for (int i = 0; i < triangle.Length; i++)
+        {
+            if (!IsVertexInSameLineFromTriangle(i, triangle, originalVertices))
+            {
+                diagonalIndices.Add(triangle[i]);
+                diagonalIndicesLoopOrder.Add(backLoopVertexIndices[i]);
+            }
+            else if (backLoopVertexIndices[i] == Edge.IndiceLoopOrder.Back)
+            {
+                backLoopInSameLineIndice = triangle[i];
+            }
+        }
+        return (diagonalIndices.ToArray(),diagonalIndicesLoopOrder.ToArray(),backLoopInSameLineIndice);
+    }
+    bool IsVertexInSameLineFromTriangle(int currentTriangleIndice,int[] triangle, Vector3[] originalVertices)
+    {       
+        for (int i = 0; i < triangle.Length; i++)
+        {
+                if (IsOriginalVertexInSameLine(originalVertices[currentTriangleIndice], originalVertices[triangle[i]]) && currentTriangleIndice != triangle[i]) return true;
+        }
+        return false;
+    }
+    int GetVertexInSameLineFromTriangle(int currentTriangleIndice, int[] triangle, Vector3[] originalVertices)
+    {
+        for (int i = 0; i < triangle.Length; i++)
+        {
+            if (IsOriginalVertexInSameLine(originalVertices[currentTriangleIndice], originalVertices[triangle[i]]) && currentTriangleIndice != triangle[i])
+            {
+                return triangle[i];
+            }
+        }
+        return 0;
     }
     bool IsOriginalVertexInSameLine(Vector3 a,Vector3 b)
     {
@@ -117,14 +181,19 @@ public class MeshCapture
     }
     VertexData GetMatchingVertexData(VertexData[] vertexDatas, int triangle)
     {
+        VertexData vertexData = null;
         for (int i = 0; i < vertexDatas.Length; i++)
         {
             if(triangle == vertexDatas[i].originalVertexIndex)
             {
-                return vertexDatas[i];
+                vertexData = vertexDatas[i];
             }
         }
-        return null;
+        if(vertexData == null)
+        {
+            Debug.Log("null");
+        }
+        return vertexData;
     }
     int[] GetPrecomputedTrianglesArray(int loopCount, EdgeLoop edgeLoop)
     {
@@ -142,28 +211,37 @@ public class MeshCapture
         return precomputedTriangles.ToArray();
     }
     int[] GetQuadFromEdge(Edge edge, int loopIndex, int loopVertexDatasLength)
-    {
+    {     
         int baseBackLoop = loopVertexDatasLength * loopIndex;
         int baseFrontLoop = loopVertexDatasLength * (loopIndex + 1);
-        /*
-        int triangleIndiceFromBackLoop0 = baseBackLoop + edge.vertexDataRight.vertexIndex;
-        int triangleIndiceFromBackLoop1 = baseBackLoop + edge.vertexDataLeft.vertexIndex; 
-        int triangleIndiceFromBackLoop2 = baseFrontLoop + edge.vertexDataLeft.vertexIndex; 
+        int triangleIndiceFromBackLoop0 = edge.triangleFromBackLoop[0].vertexIndex + GetBaseLoopIndex(edge.triangleFromBackLoopOrder[0], baseBackLoop, baseFrontLoop);
+        int triangleIndiceFromBackLoop1 = edge.triangleFromBackLoop[1].vertexIndex + GetBaseLoopIndex(edge.triangleFromBackLoopOrder[1], baseBackLoop, baseFrontLoop);
+        int triangleIndiceFromBackLoop2 = edge.triangleFromBackLoop[2].vertexIndex + GetBaseLoopIndex(edge.triangleFromBackLoopOrder[2], baseBackLoop, baseFrontLoop);
 
-        int triangleIndiceFromFrontLoop0 = baseFrontLoop + edge.vertexDataLeft.vertexIndex; 
-        int triangleIndiceFromFrontLoop1 = baseFrontLoop + edge.vertexDataRight.vertexIndex; 
-        int triangleIndiceFromFrontLoop2 = baseBackLoop + edge.vertexDataRight.vertexIndex; 
-        */
+        int triangleIndiceFromFrontLoop0 = edge.triangleFromFrontLoop[0].vertexIndex + GetBaseLoopIndex(edge.triangleFromBackLoopOrder[0], baseBackLoop, baseFrontLoop);
+        int triangleIndiceFromFrontLoop1 = edge.triangleFromFrontLoop[1].vertexIndex + GetBaseLoopIndex(edge.triangleFromBackLoopOrder[1], baseBackLoop, baseFrontLoop);
+        int triangleIndiceFromFrontLoop2 = edge.triangleFromFrontLoop[2].vertexIndex + GetBaseLoopIndex(edge.triangleFromBackLoopOrder[2], baseBackLoop, baseFrontLoop);
 
         int[] quad = new int[6];
-        /*
+        
         quad[0] = triangleIndiceFromBackLoop0;
         quad[1] = triangleIndiceFromBackLoop1;
         quad[2] = triangleIndiceFromBackLoop2;
         quad[3] = triangleIndiceFromFrontLoop0;
         quad[4] = triangleIndiceFromFrontLoop1;
         quad[5] = triangleIndiceFromFrontLoop2;
-        */
+       
         return quad;
+    }
+    int GetBaseLoopIndex(Edge.IndiceLoopOrder indiceLoopOrder, int baseBackLoop, int baseFrontLoop)
+    {
+        if(indiceLoopOrder == Edge.IndiceLoopOrder.Back)
+        {
+            return baseBackLoop;
+        }
+        else
+        {
+            return baseFrontLoop;
+        }
     }
 }
